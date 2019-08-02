e2fb0bee7880b9db44552c038decf74ef2adbced
Mahesh Perla <maheshp212@gmail.com>
2019-08-02 09:03:37 +0530

    directives

--- b39/src/app/directives/directives.component.ts
+++ b39/src/app/directives/directives.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directives',
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.scss']
 })
-export class DirectivesComponent implements OnInit {
+export class DirectivesComponent  {
 
   fname = 'qshores';
   fruits = ['kiwi', 'melon', 'berry'];
+  counter = 0;
   constructor() { }
 
-  ngOnInit() {
+  callMe(){
+    console.log(this.counter);
+    this.counter++;
   }
 
 }
