2317284ce6c167ee5557ab8416143fcaaad973f7
Mahesh Perla <maheshp212@gmail.com>
2019-10-16 08:28:14 +0530

    routing

--- b41/src/app/variables/variables.component.ts
+++ b41/src/app/variables/variables.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VariablesComponent implements OnInit {
 
+  a = 2;
+  b = 3;
+  fname:string = 'qshroe';
+  gender:boolean = true;
+  interest:null = null;
+  fruits:string[] = ['kiwi', 'melon', 'berry'];
+  user:any = {tech:'ange', exp:56};
+  undf:any;
+
   constructor() { }
 
   ngOnInit() {
